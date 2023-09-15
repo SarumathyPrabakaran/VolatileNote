b55a62d56abcef1fd3f60bcedcb65596f052edeb
SarumathyPrabakaran <harinisarumathi@gmail.com>
2023-09-16 00:46:03 +0530

    working fine

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VolatileNote
 A Ephemeral Note Sharing App
+
+VolatileNote is a simple ephemeral note sharing application. A Flask and Bootstrap-based web application for secure, one-time message sharing via unique codes, leveraging AWS DynamoDB for data management.
+
+
--- app.py
+++ app.py
@@ -2,17 +2,69 @@ from flask import Flask,render_template,request,flash, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
 import string
+from boto3 import resource
+import uuid
+import config
+import os
+
+
 
 app = Flask(__name__)
 SECRET_KEY = "hello"
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///privnote.sqlite3'
+
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = os.environ.get("REGION_NAME")
+
+resource = resource(
+    'dynamodb',
+    aws_access_key_id     = AWS_ACCESS_KEY_ID,
+    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+    region_name           = REGION_NAME
+)
+
+
+
+table = resource.Table('Notes')
+
+
+def put_an_item(code, message):
+    response = table.put_item(Item={
+        'code' : code,
+        'message' : message
+    })
+
+    return response
+
+def read_from_db(code):
+        
+        response = table.get_item(
+        Key = {
+            'code' : code
+        },
+        AttributesToGet = [
+           'message' 
+        ]                      
+    )
+        return response
+
+
+def delete_from_db(code):
+        response = table.delete_item(
+        Key = {
+            'code': code
+        }
+    )
+
+        return response
+
 
 db=SQLAlchemy(app)
 
-url = None
 
 def get_num():
 
@@ -33,18 +85,7 @@ class privnote(db.Model):
 def home():
     return render_template("home.html")
 
-@app.route('/get_note',methods=["POST",'GET'])
-def get_note():
-    if request.method=="POST":
-        code = request.form.get("code")
-        print(code)
-        data = privnote.query.filter_by(num=code).first()
-        if data:
-            print(data)
-            return render_template('get.html',data = data)
-        else:
-            print("NOthing")
-    return render_template('get.html')
+
 
 @app.route("/createnote", methods=['GET','POST'])
 def create_note():
@@ -55,16 +96,44 @@ def create_note():
         mess = request.form['text_message']
         n = get_num()
         note = privnote(n,mess)
+
+        unique_id = str(uuid.uuid4())
+        print(put_an_item(unique_id, mess))
+
+
         db.session.add(note)
         db.session.commit()
 
         id1 = privnote.query.filter_by(message=mess).first().num
         
-        flash(f'{id1}')
-        print(id1)
-        #return redirect(url_for('create_note'))
+        flash(f'{unique_id}    {id1}')
+        print(unique_id)
+
     return render_template("index.html")
 
+
+
+
+# with unique code.
+@app.route('/get_note',methods=["POST",'GET'])
+def get_note():
+    if request.method=="POST":
+        code = request.form.get("code")
+        print(code)
+        data = privnote.query.filter_by(num=code).first()
+        data_dynamo = read_from_db(code)
+        if data_dynamo:
+            print(data_dynamo["Item"])
+            
+            return render_template('get.html',data = data_dynamo["Item"]["message"])
+        else:
+            print("NOthing")
+    return render_template('get.html')
+
+
+
+
+#unique URL
 @app.route("/view/<n>")
 def view(n):
     try:
